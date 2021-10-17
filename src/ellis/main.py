cb1217b3db8901b34fb1372630f42c970a5b162c
sapeint/chanku <sapeint@hashbang.sh>
2021-10-17 17:03:48 -0500

    Make Ellis actually use configured host and port

--- src/ellis/main.py
+++ src/ellis/main.py
@@ -5,19 +5,21 @@ and organize what is necessary. This may be modified.
 
 import logging
 
-from ellis import ellis
+from ellis import ellis, config
 
 LOG_FORMAT = '%(asctime)s - %(threadName)s :: %(message)s'
 
 if __name__ == "__main__":
-    server = ellis.EllisServer()
-    server.log.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     file_handler = logging.FileHandler("./output")
     handler.setLevel(logging.DEBUG)
     file_handler.setLevel(logging.DEBUG)
     handler.setFormatter(logging.Formatter(LOG_FORMAT))
     file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    config.read_in()
+    server = ellis.EllisServer(hostname=config.Config['Core']['Hostname'],
+                               port=int(config.Config['Core']['Port']))
+    server.log.setLevel(logging.DEBUG)
     server.log.addHandler(handler)
     server.log.addHandler(file_handler)
     server.start()
